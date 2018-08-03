437f31e757ffc08d420d90ed05ec0d47e1cfd8a7
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 22:58:52 +0530

    Chapter 8 - Makin' objects.
    refactor test: change type of reference variable five to Money.
    
    test doesnt compile.

--- src/test/java/example/tdd/MoneyTest.java
+++ src/test/java/example/tdd/MoneyTest.java
@@ -8,7 +8,7 @@ public class MoneyTest {
 
     @Test
     public void testMultiplication() {
-        Dollar five = Money.dollar(5);
+        Money five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
