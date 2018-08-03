c59b2a6b129f9af51306b31d4a5bef1032bdef31
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 22:33:20 +0530

    Chapter 6 - Equality for All, Redux.
    refactor: move equals in Dollar to Money.
    test passes.

--- src/main/java/example/tdd/Dollar.java
+++ src/main/java/example/tdd/Dollar.java
@@ -10,8 +10,4 @@ public class Dollar extends Money {
         return new Dollar(amount * multiplier);
     }
 
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount;
-    }
 }
--- src/main/java/example/tdd/Money.java
+++ src/main/java/example/tdd/Money.java
@@ -2,4 +2,9 @@ package example.tdd;
 
 public class Money {
     protected int amount;
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
 }
