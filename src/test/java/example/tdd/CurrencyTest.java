1e7b2cdb9e34947b64a7eff2f5b99f8e8060cff8
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 22:09:06 +0530

    Chapter 5 - Franc-ly Speaking. first add tests for Franc. test fails.

--- src/test/java/example/tdd/CurrencyTest.java
+++ src/test/java/example/tdd/CurrencyTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class DollarTest {
+public class CurrencyTest {
 
     @Test
     public void testMultiplication() {
@@ -17,5 +17,14 @@ public class DollarTest {
     public void testEquality() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(6));
+    }
+
+    @Test
+    public void testFrancMultiplication(){
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+
+
     }
 }
