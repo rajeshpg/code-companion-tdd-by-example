9f51daa787c0606f66555742430c7ec5d2babd03
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 19:46:55 +0530

    Chapter 2 - Degenarate Objects
    test fail. refactor to eliminate side effects.

--- src/test/java/example/tdd/DollarTest.java
+++ src/test/java/example/tdd/DollarTest.java
@@ -11,5 +11,7 @@ public class DollarTest {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
     }
 }
