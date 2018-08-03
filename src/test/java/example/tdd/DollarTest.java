c14d0cbcde285a5b265dd04fe8a9adfe942f0281
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 19:33:58 +0530

    Chapter 1 - Multi-Currency Money
    failing test. test does not compile.

---
+++ src/test/java/example/tdd/DollarTest.java
@@ -0,0 +1,15 @@
+package example.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DollarTest {
+
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
