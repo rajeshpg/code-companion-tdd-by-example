9094d8fd9550e66e7225fb80b3e6c8b81a878ccf
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 22:32:14 +0530

    Chapter 6 - Equality for All, Redux.
    refactor equals method in Dollar.
    test passes.

--- src/main/java/example/tdd/Dollar.java
+++ src/main/java/example/tdd/Dollar.java
@@ -11,7 +11,7 @@ public class Dollar extends Money {
     }
 
     public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
+        Money money = (Money) object;
+        return amount == money.amount;
     }
 }
