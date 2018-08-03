bafde61143c9990844e76a21c43e411bfa23b5db
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 20:02:16 +0530

    Chapter 2 - Degenarate Objects. refactor to eliminate side effects.
    test compiles but fails.

--- src/main/java/example/tdd/Dollar.java
+++ src/main/java/example/tdd/Dollar.java
@@ -6,7 +6,8 @@ public class Dollar {
         this.amount = amount;
     }
 
-    void times(int multiplier){
+    Dollar times(int multiplier){
         amount *= multiplier;
+        return null;
     }
 }
