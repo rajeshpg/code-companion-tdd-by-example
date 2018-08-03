ff7a795f055363b2e92becc778056badac8c52bc
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 22:14:29 +0530

    Chapter 5 - Franc-ly Speaking.
    implement Franc.
    
    test passes.

---
+++ src/main/java/example/tdd/Franc.java
@@ -0,0 +1,19 @@
+package example.tdd;
+
+public class Franc {
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return amount == franc.amount;
+    }
+
+}
