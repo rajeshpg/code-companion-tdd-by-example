586645b4a6a43c057ca25c00f3a94cec71e4bef6
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 22:38:21 +0530

    Chapter 6 - Equality for All, Redux.
    make Franc as subclass of Money.
    remove equals method in Franc
    test passes.

--- src/main/java/example/tdd/Franc.java
+++ src/main/java/example/tdd/Franc.java
@@ -1,7 +1,6 @@
 package example.tdd;
 
-public class Franc {
-    private int amount;
+public class Franc extends Money {
 
     public Franc(int amount) {
         this.amount = amount;
@@ -9,11 +8,6 @@ public class Franc {
 
     Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
     }
 
 }
