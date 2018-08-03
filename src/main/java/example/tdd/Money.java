6c402ea08985b91f5c04388eab2fd2a504ab86e9
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 22:58:19 +0530

    Chapter 8 - Makin' objects.
    refactor Money: add static factory method to create Dollar object.

--- src/main/java/example/tdd/Money.java
+++ src/main/java/example/tdd/Money.java
@@ -3,6 +3,10 @@ package example.tdd;
 public class Money {
     protected int amount;
 
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
