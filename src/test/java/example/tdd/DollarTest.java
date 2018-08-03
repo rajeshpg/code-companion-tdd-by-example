fe0e5cc338f691711ad1c7ed3df8984ec9873e1b
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-03 21:05:20 +0530

    Chapter 4 - Privacy.
    refactor tests :-)

--- src/test/java/example/tdd/DollarTest.java
+++ src/test/java/example/tdd/DollarTest.java
@@ -9,10 +9,8 @@ public class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(new Dollar(10), product);
-        product = five.times(3);
-        assertEquals(new Dollar(15), product);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
