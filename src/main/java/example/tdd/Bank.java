152e6951a09e5dd4fa59f9cd7b88bf192bbef987
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 13:49:35 +0530

    Chapter 14 - Change
    add Bank as a parameter in Expression.reduce and its implementations.

--- src/main/java/example/tdd/Bank.java
+++ src/main/java/example/tdd/Bank.java
@@ -3,7 +3,7 @@ package example.tdd;
 public class Bank {
 
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     public void addRate(String chf, String usd, int i) {
