04f5f70dfd04f7447502e193817032737acd7ff5
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 13:46:31 +0530

    Chapter 14 - Change
    refactor Money and Bank.

--- src/main/java/example/tdd/Bank.java
+++ src/main/java/example/tdd/Bank.java
@@ -5,4 +5,8 @@ public class Bank {
     Money reduce(Expression source, String to) {
         return source.reduce(to);
     }
+
+    public void addRate(String chf, String usd, int i) {
+
+    }
 }
