68bf17ea7e6408ae56f624532fce6236a28029b8
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 13:38:50 +0530

    Chapter 13 - Make It
    declare reduce in Expression

--- src/main/java/example/tdd/Bank.java
+++ src/main/java/example/tdd/Bank.java
@@ -3,9 +3,6 @@ package example.tdd;
 public class Bank {
 
     Money reduce(Expression source, String to) {
-        if (source instanceof Money)
-            return (Money) source.reduce(to);
-        Sum sum = (Sum) source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
 }
