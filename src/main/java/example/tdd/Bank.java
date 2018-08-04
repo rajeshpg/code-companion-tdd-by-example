189c6f8c71c0cc0be400b7e2d149f5b132db3f6d
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 13:07:30 +0530

    Chapter 13 - Make It
    refactor reduce() in Bank

--- src/main/java/example/tdd/Bank.java
+++ src/main/java/example/tdd/Bank.java
@@ -3,6 +3,8 @@ package example.tdd;
 public class Bank {
 
     Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        Sum sum = (Sum) source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount,to);
     }
 }
