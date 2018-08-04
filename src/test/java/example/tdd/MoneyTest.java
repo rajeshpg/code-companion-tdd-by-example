0763259518929e5ee83ff40ea7d57dfd5d1e70ee
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 12:51:50 +0530

    Chapter 13 - Make It
    add test for Sum

--- src/test/java/example/tdd/MoneyTest.java
+++ src/test/java/example/tdd/MoneyTest.java
@@ -36,4 +36,13 @@ public class MoneyTest {
         assertEquals(Money.dollar(10), reduced);
     }
 
+    @Test
+    public void testPlusReturnsSum() {
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        assertEquals(five, sum.augend);
+        assertEquals(five, sum.addend);
+    }
+
 }
