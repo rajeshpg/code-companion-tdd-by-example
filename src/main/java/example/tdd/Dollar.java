535a8d0a8d57d02e8161ae623393d47ab18a6933
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 10:52:46 +0530

    Chapter 10 - Interesting Times
    use constructors in times().
    go backward to go forward.

--- src/main/java/example/tdd/Dollar.java
+++ src/main/java/example/tdd/Dollar.java
@@ -12,7 +12,7 @@ public class Dollar extends Money {
     }
 
     public Money times(int multiplier) {
-        return Money.dollar(amount * multiplier);
+        return new Dollar(amount * multiplier, currency);
     }
 
 }
