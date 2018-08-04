f186cf30a7a85a492cf979e0a0460e0362c74009
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 11:28:00 +0530

    Chapter 11 - The Root of All Evil.
    refactor: delete Dollar and Franc.
    remove unnecessary tests and assertions.

--- src/test/java/example/tdd/MoneyTest.java
+++ src/test/java/example/tdd/MoneyTest.java
@@ -17,16 +17,7 @@ public class MoneyTest {
     public void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(6));
-        assertEquals(Money.franc(5), Money.franc(5));
-        assertNotEquals(Money.franc(5), Money.franc(6));
         assertNotEquals(Money.franc(5), Money.dollar(5));
-    }
-
-    @Test
-    public void testFrancMultiplication(){
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
@@ -35,8 +26,4 @@ public class MoneyTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
-    @Test
-    public void testDifferentClassEquality() {
-        assertEquals(new Money(10, "CHF"), new Franc(10, "CHF"));
-    }
 }
