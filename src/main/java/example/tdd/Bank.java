4c13b269177190256dbfa199824dff11945e0b8d
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 13:18:59 +0530

    Chapter 13 - Make It
    ugly fix to make test pass.

--- src/main/java/example/tdd/Bank.java
+++ src/main/java/example/tdd/Bank.java
@@ -3,6 +3,7 @@ package example.tdd;
 public class Bank {
 
     Money reduce(Expression source, String to) {
+        if (source instanceof Money) return (Money) source;
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
--- src/test/java/example/tdd/MoneyTest.java
+++ src/test/java/example/tdd/MoneyTest.java
@@ -47,10 +47,9 @@ public class MoneyTest {
 
     @Test
     public void testReduceSum() {
-        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
         Bank bank = new Bank();
-        Money result = bank.reduce(sum, "USD");
-        assertEquals(Money.dollar(7), result);
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
     }
 
 }
