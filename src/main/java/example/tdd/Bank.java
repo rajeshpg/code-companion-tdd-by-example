ffcb5e1194771c7308fc58a4d7687b22a43802e0
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 14:04:48 +0530

    Chapter 14 - Change
    refactor Bank to handle rates.

--- src/main/java/example/tdd/Bank.java
+++ src/main/java/example/tdd/Bank.java
@@ -1,16 +1,43 @@
 package example.tdd;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
 
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
 
-    public void addRate(String chf, String usd, int i) {
-
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 
     public int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+
+    private class Pair {
+        private String from;
+        private String to;
+
+        public Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            Pair pair = (Pair) object;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
     }
 }
