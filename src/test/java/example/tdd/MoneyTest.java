84a572896ddd10d21a2730d67eb3ad6d93a644dc
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 12:42:08 +0530

    Chapter 13 - Make It
    refactor test to include Objects that handle expressions and conversions.

--- src/test/java/example/tdd/MoneyTest.java
+++ src/test/java/example/tdd/MoneyTest.java
@@ -29,7 +29,10 @@ public class MoneyTest {
 
     @Test
     public void testSimpleAddition() {
-        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(10), sum);
     }
 
