12b44d231b7fee45a586d75fcc97c9f2e32b0660
Rajesh Pitty <rajesh@rajeshpg.com>
2018-08-04 13:40:13 +0530

    Chapter 14 - Change
    test currency conversion

--- src/test/java/example/tdd/MoneyTest.java
+++ src/test/java/example/tdd/MoneyTest.java
@@ -52,4 +52,12 @@ public class MoneyTest {
         assertEquals(Money.dollar(1), result);
     }
 
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF","USD",2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
 }
